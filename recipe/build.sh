487f7013017316754a6c6574d96471ce7abf1fa6
Huidae Cho <grass4u@gmail.com>
2025-09-26 10:20:54 -0600

    Generate post-link.sh dynamically to replace compile-time path with install-time path

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,3 +7,6 @@ cmake .. > cmake.log
 make -j$(nproc) > make.log
 
 cp -a output/* $PREFIX
+
+sed "s#@compile_path@#$PREFIX#g" < $RECIPE_DIR/post-link.sh.in > $PREFIX/.conda/post-link.sh
+chmod +x $PREFIX/.conda/post-link.sh
--- recipe/post-link.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sed -Ei 's#(GISBASE = os.path.normpath\(").*("\))#\1'"$PREFIX/lib64/grass85"'\2#' $PREFIX/bin/grass
