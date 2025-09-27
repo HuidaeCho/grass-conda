d37951df13e6fc8473741c7988f12681a0731b0d
Huidae Cho <grass4u@gmail.com>
2025-09-27 06:44:14 -0600

    Replace compile prefix in other text files

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,8 @@ cp -a output/* $PREFIX
 mv $PREFIX/lib64 $PREFIX/opt
 mv $PREFIX/opt/grass85 $PREFIX/opt/grass
 
-compile_path=$(echo $PREFIX | sed -E "s#_h_env_placehold[^/]+#work/build/output#")
-
-sed "s#@compile_path@#$compile_path#g" < $RECIPE_DIR/post-link.sh.in > $PREFIX/bin/.grass-post-link.sh
+compile_prefix=$(echo $PREFIX |
+	sed -E "s#_h_env_placehold[^/]+#work/build/output#")
+sed "s#@compile_prefix@#$compile_prefix#g" < $RECIPE_DIR/post-link.sh.in \
+	> $PREFIX/bin/.grass-post-link.sh
 chmod +x $PREFIX/bin/.grass-post-link.sh
