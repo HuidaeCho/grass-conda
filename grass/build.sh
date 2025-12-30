712fe80754b0dbbf5dde9e2623dce339a29d1f2d
Huidae Cho <grass4u@gmail.com>
2025-12-30 16:39:45 -0700

    Sync with staged-recipe

--- grass/build.sh
+++ grass/build.sh
@@ -3,6 +3,12 @@ set -e
 
 case "$target_platform" in
 osx-*)
+	# pdal requires this define
+	# $PREFIX/include/pdal/FileSpec.hpp:60:22: error: 'path' is
+	# unavailable: introduced in macOS 10.15 unknown - see
+	# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+	[ "$target_platform" = "osx-64" ] &&
+		CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 	with_others="
 		--with-opengl=osx
 		--with-x=no
@@ -10,6 +16,7 @@ osx-*)
 	;;
 esac
 
+CXXFLAGS="$CXXFLAGS" \
 ./configure \
 	--prefix=$PREFIX \
 	--with-blas \
@@ -20,7 +27,8 @@ esac
 	--with-postgres \
 	--with-pthread \
 	--with-readline \
-	$with_others
+	$with_others ||
+	(echo "===== config.log =====" && cat config.log && exit 1)
 
 # ignore system built-in libiconv and use conda libiconv; avoid using
 # non-portable sed -i
