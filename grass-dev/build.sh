3341d27de7fe544bda1744676cf302a043ab9793
Huidae Cho <grass4u@gmail.com>
2026-01-04 16:48:40 -0700

    Add -D_LIBCPP_DISABLE_AVAILABILITY for osx-64 for grass-dev

--- grass-dev/build.sh
+++ grass-dev/build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-cmake -B build --install-prefix $PREFIX &&
-	cmake --build build -j$CPU_COUNT &&
-	cmake --install build
+set -e
+
+# pdal requires this define
+# $PREFIX/include/pdal/FileSpec.hpp:60:22: error: 'path' is unavailable:
+# introduced in macOS 10.15 unknown - see
+# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+[ "$target_platform" = "osx-64" ] &&
+	CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
+cmake -B build \
+	-DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+	--install-prefix $PREFIX
+cmake --build build -j$CPU_COUNT
+cmake --install build
