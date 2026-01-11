e87bcca8ca42a445660797fc4568a3096c2ee5e1
Huidae Cho <grass4u@gmail.com>
2026-01-10 20:19:54 -0700

    Avoid using system python

--- grass-dev/build.sh
+++ grass-dev/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# to avoid using system python
+export PATH="$BUILD_PREFIX/bin:$PREFIX/bin:$PATH"
+
 cmake -B build --install-prefix $PREFIX \
 	-DCMAKE_CXX_FLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
 cmake --build build -j$CPU_COUNT
