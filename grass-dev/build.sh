2830f5fd68d08cae98fe961ef1eb6aa1a67117fe
Huidae Cho <grass4u@gmail.com>
2026-01-10 20:52:09 -0700

    Try Python3_EXECUTABLE

--- grass-dev/build.sh
+++ grass-dev/build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-# to avoid using system python
-export PATH="$BUILD_PREFIX/bin:$PREFIX/bin:$PATH"
-
 cmake -B build --install-prefix $PREFIX \
+	-DPython3_EXECUTABLE=$PYTHON \
 	-DCMAKE_CXX_FLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
 cmake --build build -j$CPU_COUNT
 cmake --install build
