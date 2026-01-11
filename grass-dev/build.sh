812238935a554cab59747a06bf7752f28eec9d12
Huidae Cho <grass4u@gmail.com>
2026-01-10 17:14:57 -0700

    Sync meta.yaml

--- grass-dev/build.sh
+++ grass-dev/build.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 set -e
 
-# pdal requires this define
-# $PREFIX/include/pdal/FileSpec.hpp:60:22: error: 'path' is unavailable:
-# introduced in macOS 10.15 unknown - see
-# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-[ "$target_platform" = "osx-64" ] &&
-	CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-
-cmake -B build -DCMAKE_CXX_FLAGS="$CXXFLAGS" --install-prefix $PREFIX
+cmake -B build --install-prefix $PREFIX \
+	-DCMAKE_CXX_FLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
 cmake --build build -j$CPU_COUNT
 cmake --install build
