8e2b52d463ad9defa3e1dafd53d74937e039f700
Huidae Cho <grass4u@gmail.com>
2026-04-27 18:06:29 -0600

    Test Windows build dir

--- grass-dev/bld.bat
+++ grass-dev/bld.bat
@@ -6,5 +6,13 @@ set CMAKE_BUILD_PARALLEL_LEVEL=1
 cmake --build build --config Release -j%CPU_COUNT% --verbose
 if errorlevel 1 exit /b 1
 
+echo "===== after build ====="
+
+cd build
+dir/w/s *.bat
+cd ..
+
+echo "===== before install ====="
+
 cmake --install build --config Release
 if errorlevel 1 exit /b 1
